cd7d9567aa959c7f0dcca8b819a08e80152a1c58
unknown <akdho545@IFMPRE102.ad.liu.se>
2014-04-28 16:31:35 +0200

    fixat smågrejer

--- Mätprogram/write_read_data_daq.m
+++ Mätprogram/write_read_data_daq.m
@@ -1,15 +1,14 @@
 function [measurement_matrix, Time]=write_read_data_daq(number_of_points,start_voltage,stop_voltage,R)
 
-% sessionen ska tas bort ur koden efter att mätprogramdelen testats klart
-s = daq.createSession('ni')
-ai0=s.addAnalogInputChannel('cDAQ1Mod3',0,'Voltage')
-ai0.TerminalConfig = 'SingleEnded'
+s = daq.createSession('ni');
 
-ai1=s.addAnalogInputChannel('cDAQ1Mod3',1,'Voltage')
-ai1.TerminalConfig = 'SingleEnded'
+ai0 = s.addAnalogInputChannel('cDAQ1Mod3',0,'Voltage');
+ai0.TerminalConfig = 'SingleEnded';
+
+
 
 s.addAnalogOutputChannel('cDAQ1Mod1',0,'Voltage')
-s.addAnalogOutputChannel('cDAQ1Mod1',1,'Voltage')
+
 
 Time = linspace(start_voltage,stop_voltage, number_of_points)';
 output_data = Time+start_voltage;
@@ -17,28 +16,20 @@ output_data = Time+start_voltage;
 figure(1)
 plot(output_data);
 title('Output Data Queued');
-
-output_data2 =output_data;
-s.queueOutputData([output_data output_data2])
-figure(3)
-plot(output_data2);
-title('Output Data2 Queued');
+s.queueOutputData([output_data]);
 
 [captured_data,time] = s.startForeground();
 figure(2)
-plot(time,captured_data(:,1));
+plot(time,captured_data);
 ylabel('Voltage');
 xlabel('Time');
 title('Acquired Signal');
 
-figure(4)
-plot(time,captured_data(:,2));
-ylabel('Voltage');
-xlabel('Time');
-title('Acquired Signal');
 
 measurement_matrix=captured_data;
 measurement_matrix(:,2)=measurement_matrix(:,2)/R;
+
+
 
 end
 
