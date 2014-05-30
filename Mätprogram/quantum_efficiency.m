cf9249b24f713aa6e32af132fd4ef25cbc41bce5
Olgitt <olgittmar@gmail.com>
2014-05-30 11:31:47 +0200

    modified the testProgram GO so that it can do some more stuff

--- Mätprogram/quantum_efficiency.m
+++ Mätprogram/quantum_efficiency.m
@@ -21,23 +21,28 @@ try
     
     figure(2)
     plot(Y)
+    axis([280 500 0 200]);
     
-    [PKS, LOCS] = findpeaks(abs(Y),'THRESHOLD', 0.001);
-    PKS
-    LOCS
-    filtered_PKS = zeros(1,16);
-  
-        for i = 1:16
-            tmp = abs(LOCS - frequencies(i));
-            if min(tmp)<5
-            [~, idx] = min(tmp - 1);
-            if length(idx) > 1
-                idx = idx(1);
-            end
-            filtered_PKS(1,i) = PKS(idx)
-            end
-        end
-  
+%     [PKS, LOCS] = findpeaks(abs(Y),'THRESHOLD', 0.001);
+%     PKS
+%     LOCS
+     filtered_PKS = zeros(1,16);
+%   
+%         for i = 1:16
+%             tmp = abs(LOCS - frequencies(i));
+%             if min(tmp)<5
+%             [~, idx] = min(tmp - 1);
+%             if length(idx) > 1
+%                 idx = idx(1);
+%             end
+%             filtered_PKS(1,i) = PKS(idx)
+%             end
+%         end
+
+
+for i = 1:length(frequencies)
+    filtered_PKS(1,i) = Y(frequencies(i));
+end
     
     figure(3)
     scatter(frequencies,filtered_PKS)
@@ -46,32 +51,38 @@ try
     currentIntensity = (1/(0.83*5))*[2.5 2.9 1.3 3.8 4.5 3 1.9 4.3 4.7 4.5 3.8 2.6 4.6 2.8 3 4.6];
     
     %konvertera ström till total effekt, DUBBELKOLLA!!
-    P = (getappdata(handles.figure1, 'quantum_spectrum')*(1.8).*currentIntensity*getappdata(handles.figure1,'illuminated_area'));
-    
+    Qspectrum = getappdata(handles.figure1, 'quantum_spectrum');
+    area = getappdata(handles.figure1,'illuminated_area');
     wavelengths = [590 720 980 830 880 945 680 520 420 450 780 630 660 750 490 515]*1e-9;
+    
+    P = Qspectrum*(1.8).*currentIntensity*area;
     h = 6.6261e-34;
     c = 3e8;
-    photons = P./((h*c)./wavelengths);
+    energy = (h*c)./wavelengths;
+    photons = P./energy;
     
     %konvertera ström till antal elektroner
     electrons = filtered_PKS/(1.602e-19);
     
-    QE = round(electrons./photons);
-    
+    QE = electrons./photons;
+    faults = 0;
     for i = 1:length(QE)
         if(QE(i) == inf)
             QE(i) = 0;
+        elseif QE(i) > 1
+            faults = faults + 1;
+            disp(strcat('QE = ',num2str(QE(i))));            
+            disp(strcat('electrons = ',num2str(electrons(i))));
+            disp(strcat('photons = ', num2str(photons(i))));
         end
     end
-    
-    axes(handles.axes1);
-    
+    disp(strcat('faults = ', num2str(faults)));
     light_wavelengths = [420 450 490 515 520 590 630 660 680 720 750 780 830 880 945 980];
     wavelength_efficiency = [QE(9) QE(10) QE(15) QE(16) QE(8) QE(1) QE(12) QE(13) QE(7) QE(2) QE(14) QE(11) QE(4) QE(5) QE(6) QE(3)];
-    
-    
+
+    axes(handles.axes1);
     plot(light_wavelengths, wavelength_efficiency);
-    axis([420 980 0 110]);
+    axis([420 980 0 10]);
 
 %     figure(2)
 %     plot(Y)
--- testProgram/go.m
+++ testProgram/go.m
@@ -1,4 +1,4 @@
-function go( channel, voltage, s, spec_s )
+function go( channel, voltage, s, spec_s , mode)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,21 +6,36 @@ digital = zeros(1,16);
 analog = zeros(1,15);
 spec_analog = 0;
 
-digital(channel) = 1;
-if(channel <16)
-    analog(channel) = voltage;
-else
+if strcmp(mode, 'analogOn')
+    analog = voltage*ones(1,15);
     spec_analog = voltage;
+    digital(channel) = 1;
+    
+elseif strcmp(mode, 'digitalOn')
+    if(channel <16)
+        analog(channel) = voltage;
+    else
+        spec_analog = voltage;
+    end
+    digital = ones(1,16);
+    
+else
+    if(channel <16)
+        analog(channel) = voltage;
+    else
+        spec_analog = voltage;
+    end
+    digital(channel) = 1;    
+
 end
 
 % hej = failtest([analog spec_analog])
 
 if (failtest([analog spec_analog]) == 1)
-
-    'too high current'
+    disp('too high current');
     
 else
-s.outputSingleScan([digital analog]); spec_s.outputSingleScan([spec_analog]);
-
+    s.outputSingleScan([digital analog]); spec_s.outputSingleScan([spec_analog]);
+    
 end
 
